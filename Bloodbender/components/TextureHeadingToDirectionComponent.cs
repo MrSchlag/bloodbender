ffb44f85c11a53f19e6cd002b43a909342755416
kerrad_y <yanis.kerrad@epitech.eu>
2017-07-12 00:19:36 +0200

    Correction TextureHeading

--- Bloodbender/components/TextureHeadingToDirectionComponent.cs
+++ Bloodbender/components/TextureHeadingToDirectionComponent.cs
@@ -17,8 +17,15 @@ namespace Bloodbender
 
         bool IComponent.Update(float elapsed)
         {
-            float angleLinearVelocityVector = (float)Math.Atan(owner.body.LinearVelocity.Y / owner.body.LinearVelocity.X);
-            owner.setRotation(angleLinearVelocityVector + (float)Math.PI / 2);
+            //float angleLinearVelocityVector = (float)Math.Atan(owner.body.LinearVelocity.Y / owner.body.LinearVelocity.X);
+            //owner.setRotation(angleLinearVelocityVector + (float)Math.PI / 2);
+
+            //float angleLinearVelocityVector = (float)(Math.Atan(owner.body.LinearVelocity.Y / owner.body.LinearVelocity.X) * 180 / Math.PI + 90);
+
+            float angleLinearVelocityVector = (float)(Math.Atan2(owner.body.LinearVelocity.Y, owner.body.LinearVelocity.X) - Math.Atan2(1, 0) + Math.PI / 2);
+            owner.setRotation(angleLinearVelocityVector);
+
+            Console.WriteLine(angleLinearVelocityVector);
 
             return true;
         }
