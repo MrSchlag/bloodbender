b13b7302fb549e90ca78e08cfdb7370f91b45559
oscar lambrun <oscar.lambrun@epitech.eu>
2017-07-25 23:07:41 +0200

    remove all master content

--- Bloodbender/PathFinding/NodeTriangle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bloodbender.PathFinding
-{
-    public class NodeTriangle
-    {
-        public PathFinderNode p1;
-        public PathFinderNode p2;
-        public PathFinderNode p3;
-    }
-}
--- README.md
+++
@@ -1,7 +0,0 @@
-# bloodbender
-Rogue like game
-
-stack:
-monogame
-farseer
-tiled
