c994c419e77ede4ca19707a1c0c80aaf2ee0924b
oscar lambrun <oscar.lambrun@epitech.eu>
2017-08-06 17:59:47 +0200

    remove sharpnoise

--- Bloodbender/TreePlanter.cs
+++ Bloodbender/TreePlanter.cs
@@ -1,10 +1,5 @@
-﻿using FarseerPhysics.Dynamics;
-using Microsoft.Xna.Framework;
-using System;
+﻿using Microsoft.Xna.Framework;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bloodbender
 {
