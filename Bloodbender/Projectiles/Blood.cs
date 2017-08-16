f93823d7461dcb5f2a7f2b81e2e2756d2420473c
kerrad_y <yanis.kerrad@epitech.eu>
2017-08-16 21:01:00 +0200

    push to pull

---
+++ Bloodbender/Projectiles/Blood.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloodbender.Projectiles
+{
+    class Blood : Projectile
+    {
+        public Blood(Vector2 position, float radius, float angle, float speed) : base(position, radius, angle, speed)
+        {
+            offSet = OffSet.Center;
+
+            Animation anim = new Animation(Bloodbender.ptr.Content.Load<Texture2D>("blood"), 4, 0.05f, 32, 0, 0, 0);
+            anim.reset();
+            addAnimation(anim);
+        }
+    }
+}
---
+++ Bloodbender/Projectiles/LanceGobelin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloodbender.Projectiles
+{
+    class LanceGobelin : Projectile
+    {
+        public LanceGobelin(Vector2 position, float radius, float angle, float speed) : base(position, radius, angle, speed)
+        {
+            offSet = OffSet.Center;
+
+            Animation anim = new Animation(Bloodbender.ptr.Content.Load<Texture2D>("GangMinion/spear"));
+            anim.reset();
+            addAnimation(anim);
+        }
+    }
+}
