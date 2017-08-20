a1cfe002f36b85cd72810ec1c5bd95706dfa53fe
oscar lambrun <oscar.lambrun@club-internet.fr>
2017-08-20 16:29:07 +0200

    add keepdistancecomponent

--- Bloodbender/Enemies/Scenario2/PartnerFar.cs
+++ Bloodbender/Enemies/Scenario2/PartnerFar.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Bloodbender.components;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Bloodbender.Enemies.Scenario2
@@ -6,6 +7,8 @@ namespace Bloodbender.Enemies.Scenario2
     public class PartnerFar : Enemy
     {
         public PartnerClose Partner { get; set; }
+        private PhysicObj _node;
+        private readonly float _initialVelocity = 50;
 
         public PartnerFar(Vector2 position, PhysicObj target) : base(position, target)
         {
@@ -14,22 +17,39 @@ namespace Bloodbender.Enemies.Scenario2
             Animation anim = new Animation(Bloodbender.ptr.Content.Load<Texture2D>("bouleRouge"));
             addAnimation(anim);
 
+            Animation attackAnimation = new Animation(Bloodbender.ptr.Content.Load<Texture2D>("GangMinion/goblinattack"), 7, 0.1f, 0, 0, 0, 0);
+            attackAnimation.isLooping = false;
+            addAnimation(attackAnimation);
+
             Bloodbender.ptr.shadowsRendering.addShadow(new Shadow(this));
 
             fixture = createOctogoneFixture(50f, 50f, Vector2.Zero, new AdditionalFixtureData(this, HitboxType.BOUND));
             Radius = 50f;
-            velocity = 50;
+            velocity = _initialVelocity;
 
             addFixtureToCheckedCollision(fixture);
 
-            //IComponent comp = new FollowBehaviorComponent(this, target, 100f);
-            //addComponent(comp);
-            //IComponent comp = new
+            _node = new PhysicObj(new Vector2(position.X, position.Y), PathFinderNodeType.CENTER);
+            _node.createOctogoneFixture(10, 10, Vector2.Zero, new AdditionalFixtureData(_node, HitboxType.BOUND)).IsSensor = true;
+            _node.Radius = 10.0f;
+
+            IComponent comp = new KeepDistanceComponent(300, Bloodbender.ptr.player, _node);
+            _node.addComponent(comp);
+
+            IComponent comp2 = new FollowBehaviorComponent(this, _node, 0);
+            addComponent(comp2);
+
+            canAttack = true;
+            canGenerateProjectile = false;
+            canBeHitByPlayer = false;
+            canBeHitByProjectile = false;
         }
 
         public override bool Update(float elapsed)
         {
+            _node.Update(elapsed);
             return base.Update(elapsed);
         }
+        
     }
 }
--- Bloodbender/components/KeepDistanceComponent.cs
+++ Bloodbender/components/KeepDistanceComponent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,8 +23,53 @@ namespace Bloodbender.components
         public bool Update(float elapsed)
         {
             if ((_target.body.Position - _owner.body.Position).Length() < _distance)
+            {
+                var closestEscapePoint = FindClosestEscapePoint();
+                _owner.body.Position = closestEscapePoint;
+                _owner.position = closestEscapePoint * Bloodbender.meterToPixel;
                 return false;
+            }
+            else
+            {
+
+            }
             return true;
+        }
+
+        private Vector2 FindClosestEscapePoint()
+        {
+            var vectorToRotate = new Vector2(_distance, 0);
+            List<Vector2> pointsAround = new List<Vector2>();
+            Vector2 vec;
+            int step = 10;
+            for (int i = 0; i <= 360; i += step)
+            {
+                vectorToRotate = vectorToRotate.Rotate(i * (float)Math.PI / 180f);
+                vec = vectorToRotate + _target.body.Position;
+
+                if (!TreePlanter.IsPointOutside(vec.X, vec.Y))
+                    pointsAround.Add(vec);
+            }
+
+            return ClosestPointInList(pointsAround);
+        }
+
+        private Vector2 ClosestPointInList(List<Vector2> pointsAround)
+        {
+            Vector2 closest = _owner.body.Position;
+            float closestDistance = -1f;
+
+            foreach (var vec in pointsAround)
+            {
+                var distance = (vec - _owner.body.Position).LengthSquared();
+                if (closestDistance == -1f || distance < closestDistance)
+                {
+                    closest = vec;
+                    closestDistance = distance;
+                }
+             }
+
+            return closest;
         }
 
         public void Remove()
